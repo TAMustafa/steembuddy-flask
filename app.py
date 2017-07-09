a9358f7ef1e50d2830c690e38cf41a8828f9a609
TAMustafa <tareksnews@gmail.com>
2017-07-09 10:27:51 +0200

    Flask App for interest API

---
+++ app.py
@@ -0,0 +1,18 @@
+from steemdata import SteemData
+from flask import Flask, jsonify, request 
+
+app = Flask(__name__)
+
+s = SteemData()
+
+#GET steemit.com users filtered by interest 
+@app.route('/steemians/<string:interest>')
+def show_users(interest):
+
+    steemians = list(s.Accounts.find({'json_metadata.profile.about': {'$regex': interest, '$options': "i"}}, 
+      projection={"name": 1, "_id": 0, "profile.location": 1, "profile.about": 1, "profile.profile_image": 1}))
+
+    return jsonify({'steemians': steemians})
+
+
+app.run(port=5000)
