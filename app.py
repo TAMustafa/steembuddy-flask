fd4b4d6aca4e1293741f0774a20f85b6d174e327
TAMustafa <tareksnews@gmail.com>
2017-08-03 16:31:28 +0200

    28 Day search Filter added

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+import datetime as dt
 from steemdata import SteemData
 from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin 
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app)
 
 s = SteemData()
+time_constraints = {
+    '$gte': dt.datetime.now() - dt.timedelta(days=28)}
 
 #GET steemit.com users filtered by interest 
 @app.route('/steemians/<string:interest>')
 def show_users(interest):
 
-    steemians = list(s.Accounts.find({'json_metadata.profile.about': {'$regex': interest, '$options': "i"}}, 
-      projection={"name": 1, "_id": 0, "profile.location": 1, "profile.about": 1, "profile.profile_image": 1}))
+    steemians = list(s.Accounts.find({'json_metadata.profile.about': {'$regex': interest, '$options': "i"}, 'last_post': time_constraints},
+      projection={'name': 1, '_id': 0, 'profile.location': 1, 'profile.about': 1, 'last_post': 1,'profile.profile_image': 1}))
 
     return jsonify({'steemians': steemians})
 
